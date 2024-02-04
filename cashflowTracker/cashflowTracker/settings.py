eae41c08532e1b7ce013418f1011004928649a6b
Efrain Palencia <epalen6@wgu.edu>
2024-02-04 17:50:32 -0500

    Migrates test db

--- cashflowTracker/cashflowTracker/settings.py
+++ cashflowTracker/cashflowTracker/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'cashflowTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '#1Dealer',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -135,5 +139,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600, ssl_require=True)
