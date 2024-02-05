7ac38c880e9e28dadcda63e2a56685d397d5e8b2
Efrain Palencia <epalen6@wgu.edu>
2024-02-04 23:58:36 -0500

    Adds budget entry form and creates transactions app

--- cashflowTracker/budget/urls.py
+++ cashflowTracker/budget/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import SignUpView
+from .views import CreateBudgetEntryView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("create_budget/", CreateBudgetEntryView.as_view(),
+         name="create_budget_entry"),
+
 ]
--- cashflowTracker/budget/views.py
+++ cashflowTracker/budget/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views import View
 
+from .forms import BudgetForm
 from .forms import CustomUserCreationForm
 
 
@@ -8,3 +11,23 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class CreateBudgetEntryView(View):
+    template_name = "create_budget.html"
+
+    def get(self, request, *args, **kwargs):
+        form = BudgetForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = BudgetForm(request.POST)
+        if form.is_valid():
+            # Process the form data and save it to the database
+            budget_entry = form.save(commit=False)
+        # Assuming you have a user associated with the budget entry
+            budget_entry.user = request.user
+            budget_entry.save()
+        # Redirect to the home page or another appropriate view
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
